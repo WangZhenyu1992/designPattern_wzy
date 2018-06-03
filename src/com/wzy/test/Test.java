4932c2df671567b19062289d9c40e3264c49657d
wangzhenyu <daofei8754@126.com>
2018-06-03 20:40:58 +0800

    github push test

---
+++ src/com/wzy/test/Test.java
@@ -0,0 +1,13 @@
+package com.wzy.test;
+
+/**
+ * designPattern_wzy
+ *
+ * @author wangzhenyu
+ * @create 2018-06-03 20:37
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
