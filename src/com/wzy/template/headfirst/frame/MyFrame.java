fedebad41dae749ac96e7c1b9e92ef83fc9e6c43
wangzhenyu <daofei8754@126.com>
2018-06-06 17:28:53 +0800

    模板方法模式学习代码

--- src/com/wzy/template/headfirst/frame/MyFrame.java
+++ src/com/wzy/template/headfirst/frame/MyFrame.java
@@ -4,23 +4,23 @@ import java.awt.*;
 import javax.swing.*;
 
 public class MyFrame extends JFrame {
-	private static final long serialVersionUID = 2L;
+    private static final long serialVersionUID = 2L;
 
-	public MyFrame(String title) {
-		super(title);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public MyFrame(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		this.setSize(300,300);
-		this.setVisible(true);
-	}
+        this.setSize(300, 300);
+        this.setVisible(true);
+    }
 
-	public void paint(Graphics graphics) {
-		super.paint(graphics);
-		String msg = "I rule!!";
-		graphics.drawString(msg, 100, 100);
-	}
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+        String msg = "I rule!!";
+        graphics.drawString(msg, 100, 100);
+    }
 
-	public static void main(String[] args) {
-		MyFrame myFrame = new MyFrame("Head First Design Patterns");
-	}
+    public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame("Head First Design Patterns");
+    }
 }
