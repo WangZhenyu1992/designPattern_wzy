d45979bde52727cbc5078f9f814f63ca555c4dff
wangzhenyu <daofei8754@126.com>
2018-06-20 10:38:44 +0800

    headfirst 代理模式

---
+++ src/com/wzy/proxy/headfirst/gumball/State.java
@@ -0,0 +1,10 @@
+package com.wzy.proxy.headfirst.gumball;
+
+import java.io.*;
+  
+public interface State extends Serializable {
+	void insertQuarter();
+	void ejectQuarter();
+	void turnCrank();
+	void dispense();
+}
